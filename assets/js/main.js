01c4bafd24efb5dd483f90e886eb376e20a143da
Jaseem-v <muhammedjaseem753@gmail.com>
2021-09-21 08:10:34 +0100

    terms tab added

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,18 +67,6 @@ var swiper1 = new Swiper(".testimonial__swiper", {
     grabCursor: true,
 });
 
-// //////////////////
-//TABS
-
-$("#tabs .nav-tabs a").click(function () {
-    var position = $(this).parent().position();
-    var width = $(this).parent().width();
-    $("#tabs .slider").css({ "left": + position.left, "width": width });
-});
-var actWidth = $("#tabs .nav-tabs").find(".active").parent("li").width();
-var actPosition = $("#tabs .nav-tabs .active").position();
-$("#tabs .slider").css({ "left": + actPosition.left, "width": actWidth });
-
 
 
 // ------------mobile-menu----//
