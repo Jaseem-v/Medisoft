bd3f571813bf7fad38fe97be0fc980bcfc662558
Jaseem-v <muhammedjaseem753@gmail.com>
2021-09-28 18:56:25 +0100

    btn hover added

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,6 +127,29 @@ $(document).ready(function () {
 });
 
 
+
+////////////////////////////////////////////
+// btn hover
+$(function () {
+    $('.btn')
+        .on('mouseenter', function (e) {
+            var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+            $(this).find('span').css({ top: relY, left: relX })
+        })
+        .on('mouseout', function (e) {
+            var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+            $(this).find('span').css({ top: relY, left: relX })
+        });
+    $('[href=#]').click(function () { return false });
+});
+
+
+
+
 /////////////////////////////////////////////////
 // aos
 
